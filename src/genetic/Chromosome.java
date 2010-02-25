db7a871224f43dd287904f83ce1ca233e1096be1
Ilya G Mirin <gpdribbler@gmail.com>
2010-02-25 12:44:13 +1000

    Расширил класс хромосомы.

--- src/genetic/Chromosome.java
+++ src/genetic/Chromosome.java
@@ -3,36 +3,52 @@ package genetic;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 public class Chromosome {
-    private List<Gene> gens = new ArrayList<Gene>();
-    private Random random = new Random();
+    private int fitness = 0;
 
-    public List<Integer> getvalues() {
+    private List<Gene> genes = new ArrayList<Gene>();
+
+    public List<Integer> getValues() {
         List<Integer> result = new ArrayList<Integer>();
-        for (Iterator<Gene> it = gens.iterator(); it.hasNext();) 
+        for (Iterator<Gene> it = genes.iterator(); it.hasNext();)
             result.add(it.next().getValue());
         return result;
     }//getvalues
 
     private void mutate(double propability) {
-        for (Iterator<Gene> it = gens.iterator(); it.hasNext();) 
+        for (Iterator<Gene> it = genes.iterator(); it.hasNext();)
             it.next().mutate(propability);
     }//mutate
 
-    public Chromosome(int gens) {
-        for (int i = 0; i < gens; i++)
-            this.gens.add(new Gene());
+    public Chromosome(int genes) {
+        for (int i = 0; i < genes; i++)
+            this.genes.add(new Gene());
     }
 
     public Chromosome(int gens, int maxGeneValue) {
         for (int i = 0; i < gens; i++)
-            this.gens.add(new Gene(maxGeneValue));
+            this.genes.add(new Gene(maxGeneValue));
     }
-    
+
+    public Gene getGene(int geneNumber) {
+        return genes.get(geneNumber);
+    }
+
+    public void setGene(int geneNumber, Gene gene) {
+        genes.add(geneNumber, gene);
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+
     public void print(){
-        for (Iterator<Gene> it = gens.iterator(); it.hasNext();)
+        for (Iterator<Gene> it = genes.iterator(); it.hasNext();)
             System.out.println(it.next().toString());
     }//print
 
